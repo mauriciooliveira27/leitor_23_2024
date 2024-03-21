766da16dc63f16bb10d210384ceae8e9001ae6ba
mauriciooliver <mauriciooliver@gmail.com>
2024-03-21 08:14:41 -0400

    commit-2

--- teste.py
+++ teste.py
@@ -4,5 +4,5 @@ from model import Db_information
 db = Db_information("Termometria",3306,"localhost","leitor_termo","termometria")
 conn = data_base.Connector(db)
 
-data = conn.select_estacao_mt()
+data = conn.select_placa_secun()
 print(data)
