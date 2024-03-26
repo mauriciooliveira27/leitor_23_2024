dd60c251f98929de753c3900fdfbc637aeb5e5ae
mauriciooliver <mauriciooliver@gmail.com>
2024-03-26 07:54:48 -0400

    adicionando metodo get_cod_placa

--- task_read_temp.py
+++ task_read_temp.py
@@ -71,27 +71,36 @@ class App:
         self.conn.insert_registro_instalacao(self.registro_instal)
 
 
+    def get_cod_placa(self):
+
+        result          =   self.conn.select_placa_secund() 
+        cod_placa       =   [cod['cod_placa'] for cod in result]
+        ip_placa        =   [ip['ip'] for ip in result]
+        return cod_placa  , ip_placa
+    
+
+
     def read_temp_placa_secun(self):
 
-        data_placa = self.conn.select_data_placa_secun()
-        resultado_agrupado      =   {}
-        chave_cordoes           =   []
+        cod_placa,ip_placa      =   self.get_cod_placa()
+        data_placa              =   self.conn.select_data_placa_secun(cod_placa)#pega os dados da placa
+        resultado_agrupado      =   {}#agrupando em dicionario os canal e sensores EX: {1:[1,2,3,4,5]}
+        chave_cordoes           =   []#salvo em lista os nomes dos cordeos fisicos EX: 'Ch1S1'
 
         for item in data_placa:
-            canal           =   item['canal_placa']
-            id_sensor       =   item['sensor_placa']
-            chave_cordoes.append(item['cordao_fisico'])
+            canal           =   item['canal_placa']#pega canal 
+            id_sensor       =   item['sensor_placa']#pega o sensor
+            chave_cordoes.append(item['cordao_fisico'])#pego nomes dos cordoes fisicos
 
             if canal not in resultado_agrupado:
-                resultado_agrupado[canal] = [id_sensor]
+                resultado_agrupado[canal] = [id_sensor]#cria o dicionario , caso a chave 'canal" ainda não exista , caso contrario ele apenas adiciona o sensor a lista na linha 97
             else:
                 resultado_agrupado[canal].append(id_sensor)
 
+        lista_final     =   [{canal:sensores} for canal , sensores in resultado_agrupado.items()]#cria lista de dicionario [{1:[1,2,3,4,5]},{2:[1,2,3,4,5]},{3:[1,2,3,4,5]}] a api espera essa estrutura
+     
 
-        lista_final     =   [{canal:sensores} for canal , sensores in resultado_agrupado.items()]
-        leituras        =   []
-
-        url = 'http://192.168.15.51/api/teste/'
+        url = f'http://{ip_placa}/api/teste/'
         response = requests.post(url, json=lista_final)
 
         if response.status_code == 200:
