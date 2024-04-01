d7387a82c998651d1656fe46731012f1ecf076de
mauriciooliver <mauriciooliver@gmail.com>
2024-04-01 07:35:32 -0400

    add_TIMESLEEP

--- task_read_temp.py
+++ task_read_temp.py
@@ -12,17 +12,17 @@ import threading
 class App:
         
     def __init__(self) -> None:
-        self.db                 =       Db_information("Termometria",3306,"localhost","leitor_termo","termometria")
-        self.conn               =       data_base.Connector(self.db)
-        self.conf               =       self.conn.get_informaton_instal()
-        self.data_instal        =       json.loads(self.conf.dados)
-        self.leitor             =       Leitor_temp()
-        self.mp                 =       Multiplex3()
-        self.dt                 =       datetime
-        self.read_temp          =       True
-        self.registro_instal    =       registro_instalacao(0, self.conf.nome, self.conf.configuracao_fisica, self.dt.now(), "")
-        self.reult_placa_main = None
-        self.result_placa_secund = None
+        self.db                     =       Db_information("Termometria",3306,"localhost","leitor_termo","termometria")
+        self.conn                   =       data_base.Connector(self.db)
+        self.conf                   =       self.conn.get_informaton_instal()
+        self.data_instal            =       json.loads(self.conf.dados)
+        self.leitor                 =       Leitor_temp()
+        self.mp                     =       Multiplex3()
+        self.dt                     =       datetime
+        self.read_temp              =       True
+        self.registro_instal        =       registro_instalacao(0, self.conf.nome, self.conf.configuracao_fisica, self.dt.now(), "")
+        self.reult_placa_main       =       None
+        self.result_placa_secund    =       None
 
 
     def exe_read_temp(self):
