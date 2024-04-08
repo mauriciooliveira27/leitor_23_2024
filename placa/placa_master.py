9a1915173f1a1b507a2076f8f3dcd6f342557e72
mauriciooliver <mauriciooliver@gmail.com>
2024-04-08 14:06:39 -0400

    testando_classe_manage_2

--- placa/placa_master.py
+++ placa/placa_master.py
@@ -15,7 +15,6 @@ class PlacaMaster(PlacaAbstract,ManagerPlacaMaster):
     def read_temp(self):
         for canal in self.lista_CodSen:
             print(canal)
-            canals              =       canal.keys()
             sensores            =       list(canal.values())
             sensores_list       =       sensores[0]
     
@@ -26,6 +25,7 @@ class PlacaMaster(PlacaAbstract,ManagerPlacaMaster):
                 
                     c_int           =       int(c)
                     s_int           =       int(s)
+                    print(c_int)
                     self.mp.set_canal(c_int)
                     self.mp.set_sensor(s_int)
                     value_sensor    =       self.leitor.read_temp()
