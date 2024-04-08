ff44d940d2f4fccddb1f6316554c5cacaa00edae
mauriciooliver <mauriciooliver@gmail.com>
2024-04-08 10:03:29 -0400

    testando_classe_manage_2

--- task_read_temp.py
+++ task_read_temp.py
@@ -22,37 +22,37 @@ class ManagerObjectPlacaSlave(ManagerPlacaSlave):
 
 
 
-def save(self, data):
-    self.registro_instal.registros_temperaturas     =   json.dumps(data)
-    self.registro_instal.data                       =   self.dt.now()
-    self.conn.insert_registro_instalacao(self.registro_instal)
+# def save(self, data):
+#     self.registro_instal.registros_temperaturas     =   json.dumps(data)
+#     self.registro_instal.data                       =   self.dt.now()
+#     self.conn.insert_registro_instalacao(self.registro_instal)
 
 
-def main():
-    app = App()
-    th1 = threading.Thread(target=app.exe_read_temp)
-    th2 = threading.Thread(target=app.read_temp_placa_secun)
+# def main():
+#     app = App()
+#     th1 = threading.Thread(target=app.exe_read_temp)
+#     th2 = threading.Thread(target=app.read_temp_placa_secun)
 
-    th1.start()
-    th2.start()
+#     th1.start()
+#     th2.start()
 
-    th1.join()
-    th2.join()
+#     th1.join()
+#     th2.join()
 
-    JSON1 = app.reult_placa_main
-    JSON2 = app.result_placa_secund
+#     JSON1 = app.reult_placa_main
+#     JSON2 = app.result_placa_secund
 
-    if JSON2 != None:
-        JSON_COMPLETE   =   {**JSON1, **JSON2}
-        app.save(JSON_COMPLETE)
-        return
-    elif JSON2 == None:
-        app.save(JSON1)
-        return
+#     if JSON2 != None:
+#         JSON_COMPLETE   =   {**JSON1, **JSON2}
+#         app.save(JSON_COMPLETE)
+#         return
+#     elif JSON2 == None:
+#         app.save(JSON1)
+#         return
 
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
     
-    dt = datetime
-    if dt.now().minute == 0 and dt.now().second < 5:
-        main()
+#     dt = datetime
+#     if dt.now().minute == 0 and dt.now().second < 5:
+#         main()
